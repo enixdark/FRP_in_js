8ef285d58588f26236dd37682b307f29270b51b2
enixdark <cqshinn92@gmail.com>
2016-10-11 23:43:51 +0700

    init project

---
+++ highorderfunc/address.js
@@ -0,0 +1,11 @@
+class Address{
+  constructor(address){
+    this._address = address;
+  }
+
+  get address(){
+    return this._address;
+  }
+}
+
+module.exports = Address;
---
+++ highorderfunc/index.js
@@ -0,0 +1,16 @@
+const Student = require('./student');
+const Address = require('./address');
+const Person = require('./person');
+var pry = require('pryjs');
+
+
+var curry = new Student('Haskell', 'Curry','111-11-1111', 'Penn State');
+curry.address = new Address('US');
+var turing = new Student('Alan', 'Turing','222-22-2222', 'Princeton');
+turing.address = new Address('England');
+var church = new Student('Alonzo', 'Church','333-33-3333', 'Princeton');
+church.address = new Address('US');
+var kleene = new Student('Stephen', 'Kleene','444-44-4444', 'Princeton');
+kleene.address = new Address('US');
+eval(pry.it)
+console.log(church);
